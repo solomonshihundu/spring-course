5bca27e971bde06192e6bc0d658074b51553aeb1
Solo <shihundusolomon@gmail.com>
2020-07-24 00:14:54 +0300

    bean proxy

--- src/main/java/com/ss/springcourse/testapp/ScopeTestappApplication.java
+++ src/main/java/com/ss/springcourse/testapp/ScopeTestappApplication.java
@@ -1,6 +1,5 @@
 package com.ss.springcourse.testapp;
 
-import com.ss.springcourse.testapp.basic.BinarySearchImplementation;
 import com.ss.springcourse.testapp.scope.PersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,14 +20,6 @@ public class ScopeTestappApplication {
 		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
 
 		PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
-
-		System.out.println("##################### "+personDAO);
-
-		System.out.println("#####################  "+personDAO.getJdbcConnection());
-
-		System.out.println("#####################  "+personDAO2);
-
-		System.out.println("#####################  "+personDAO2.getJdbcConnection());
 
 		LOGGER.info("{}",personDAO);
 		LOGGER.info("{}",personDAO.getJdbcConnection());
--- src/main/java/com/ss/springcourse/testapp/scope/JdbcConnection.java
+++ src/main/java/com/ss/springcourse/testapp/scope/JdbcConnection.java
@@ -1,9 +1,13 @@
 package com.ss.springcourse.testapp.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE,proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class JdbcConnection {
 
     public JdbcConnection()
