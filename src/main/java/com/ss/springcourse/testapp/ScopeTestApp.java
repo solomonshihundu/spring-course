1e164b34bba2a64d8c6634373960d457d6455f4c
Solo <shihundusolomon@gmail.com>
2020-07-27 00:22:39 +0300

    running without spring boot

--- src/main/java/com/ss/springcourse/testapp/ScopeTestApp.java
+++ src/main/java/com/ss/springcourse/testapp/ScopeTestApp.java
@@ -3,29 +3,41 @@ package com.ss.springcourse.testapp;
 import com.ss.springcourse.testapp.springapp.scope.PersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class ScopeTestApp {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(ScopeTestApp.class);
 
 	public static void main(String[] args)
 	{
-		ApplicationContext applicationContext = SpringApplication.run(ScopeTestApp.class, args);
+		/**
+		 * application context initialization
+		 */
+		try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext())
+		{
+			applicationContext.register(ScopeTestApp.class);
+			applicationContext.refresh();
 
-		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+			/**
+			 * dao bean creation
+			 */
+			PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
 
-		PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+			PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
 
-		LOGGER.info("{}",personDAO);
-		LOGGER.info("{}",personDAO.getJdbcConnection());
+			LOGGER.info("{}", personDAO);
+			LOGGER.info("{}", personDAO.getJdbcConnection());
 
-		LOGGER.info("{}",personDAO2);
-		LOGGER.info("{}",personDAO2.getJdbcConnection());
+			LOGGER.info("{}", personDAO2);
+			LOGGER.info("{}", personDAO2.getJdbcConnection());
+		}
+
 	}
 
 }
